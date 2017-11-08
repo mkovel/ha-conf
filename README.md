70650aaefa10f7ec541a9592d2e49d40933ba088
mkovel <d.kovel@yandex.ru>
2017-11-08 05:03:41 +0300

    first HA commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ha-conf
-My experiments with Home Assistants 
+1. Burn IMG https://home-assistant.io/docs/installation/hassbian/installation/
+2. Edit wpa_supplicant.conf file for enable WIFI init
+3. default SSH cred
+~~~
+login: pi
+pass:  raspberry
+~~~
+4. copy install.sh + cnf to raspberry pi home dir of user pi
+5. run install
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+cd ~
+cat ~/.ssh/hassio.pub | cat >> ~/.ssh/authorized_keys
+
+chmod 700 .ssh
+chmod 600 .ssh/authorized_keys
+
+sudo apt-get install mc 
+sudo apt-get install htop
+sudo apt-get install bmon
+
+sudo usermod -aG homeassistant $USER
+
+sudo chmod -R g+rwX /home/homeassistant
+
+yes | cp -rf ~/cfg/* /home/homeassistant/.homeassistant/
+
+sudo chown -R homeassistant:homeassistant /srv/homeassistant
+
+sudo apt-get install vlc
+sudo usermod -a -G audio homeassistant
+
+sudo systemctl restart home-assistant@homeassistant
+
+#install hassctl
+sudo curl -o /usr/local/bin/hassctl https://raw.githubusercontent.com/dale3h/hassctl/master/hassctl && sudo chmod +x /usr/local/bin/hassctl
